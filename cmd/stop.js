6552353b7ef338ec9bd41751b09e46d9ad1d6310
jemand771 <jemand771@gmx.net>
2018-10-13 14:47:44 +0200

    modified !stop command to stop the bot (when running on server)

--- cmd/stop.js
+++ cmd/stop.js
@@ -4,6 +4,11 @@ module.exports = {
   type: 0,
   execute(refs, msg, args) {
 		console.log("stopping bot");
+		const fs = require('fs');
+		fs.unlink('running', (err) => {
+  		if (err) throw err;
+  		console.log('stop flag set');
+		});
 		msg.channel.send("Bye everyone!");
 		msg.channel.send("*goes offline*");
 		process.exit(0);
